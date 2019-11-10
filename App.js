0a434c046271c8bf153baefdefc90737d9267ad1
Raghunath Bhattacharjee <41074356+raghunath21@users.noreply.github.com>
2019-11-10 13:01:26 +0530

    Firebase data delete from web

--- App.js
+++ App.js
@@ -2,17 +2,6 @@ import React from 'react';
 import {createStackNavigator} from 'react-navigation';
 import HomePage from './components/Home/HomePage';
 
-// import * as firebase from 'firebase';
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAaCxGSoQHobRqYHMTLjOgO72SM-8V1VfY",
-//   authDomain: "react-firebase-12.firebaseapp.com",
-//   databaseURL: "https://react-firebase-12.firebaseio.com",
-//   projectId: "react-firebase-12",
-//   storageBucket: "react-firebase-12.appspot.com",
-//   messagingSenderId: "421778096881",
-// };
-// firebase.initializeApp(firebaseConfig);
-
 const App = createStackNavigator({
   HomeScreen: {
     screen: HomePage,
